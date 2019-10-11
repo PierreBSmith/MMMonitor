a5e37f5824149daa3e6586050bff5f1233e86644
Kasen Chaque <kchaque@uci.edu>
2019-10-11 02:40:53 -0700

    start

--- MMMonitor/MainWindow.xaml.cs
+++ MMMonitor/MainWindow.xaml.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -20,9 +22,27 @@ namespace MMMonitor
     /// </summary>
     public partial class MainWindow : Window
     {
+        const string configDir = "config",
+            configFile = "config.txt";
+
         public MainWindow()
         {
             InitializeComponent();
+            Directory.CreateDirectory("config");
+            if (File.Exists(Path.Combine(configDir, configFile)));
+        }
+
+        private void ChangeInstallDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            using(FolderBrowserDialog folderBrowser = new FolderBrowserDialog())
+            {
+                if(folderBrowser.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    string path = folderBrowser.SelectedPath;
+                    File.WriteAllText("config\\config.txt", path);
+                    InstallDirTextBlock.Text = path;
+                }
+            }
         }
     }
 }
